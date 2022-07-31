a17ead3004c622549a89ab185ce5bed9258b18cc
Nitesh4850 <>
2022-07-31 15:28:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+class Main {
+  public static void main(String[] args) {
+   GameFrame frame = new GameFrame();
+   // System.out.println("inside me");
+  }
+}
